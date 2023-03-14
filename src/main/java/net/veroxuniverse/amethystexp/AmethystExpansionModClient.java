cdcc1ca04de68ce18a943cbbacc3946398b0a8ad
stauplays <stauplays@gmail.com>
2023-03-14 18:53:33 +0100

    Initial commit

---
+++ src/main/java/net/veroxuniverse/amethystexp/AmethystExpansionModClient.java
@@ -0,0 +1,10 @@
+package net.veroxuniverse.amethystexp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmethystExpansionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
